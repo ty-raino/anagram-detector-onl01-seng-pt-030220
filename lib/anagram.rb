67487fdcef7c2f8505fdd1729a136d00c64e0d6b
Ty Raino <mstymetris@yahoo.com>
2020-03-28 02:35:52 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,11 +1,13 @@
 class Anagram
-  attr_accessor :word
+  attr_accessor :words
 
-  def initialize(word)
+  def initialize(words)
     @word = word
   end
 
   def match(array)
+    @words = words.each_with_object(Hash.name []) do |word, hash|
+      hash[word.chars.sort] += [word]
   end
 
 
